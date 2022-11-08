dda9bdeb2ec5923be82f41b234f20e2603759db5
Dima <livitorskripunovich@gmail.com>
2022-11-08 15:36:13 +0200

    Update: Add ' Завдання 3'

---
+++ page.py
@@ -0,0 +1,19 @@
+import statistics
+def str():
+   try:
+        list = []
+        print('To break ')
+        text = 0
+        while True:
+                text = int(input('text->'))
+                if text > 0:
+                    list.append(text)
+                else:
+                    break
+        summary = sum(list)
+        length = len(list)
+        avg = summary / length
+        print(avg)
+   except Exception as ex:
+        print(f'Error: {ex}')
+str()
